6baf57808c5e42bb82124507f81c6d7757a94e8e
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-03-02 19:06:57 -0800

    Installing dependencies & building package

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
+
+
+$('.carousel').carousel({
+  interval: 2000
+})
+
